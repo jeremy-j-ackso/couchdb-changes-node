02277aeb41d94ae5e95d6412f6dc9a51c3acef9f
Jeremy Jackson <jeremy_j@ackso.net>
2018-02-11 16:55:44 -0700

    Made outline for what to do on a change using async.waterfall()

--- couch-to-postgres/changes-to-postgres.js
+++ couch-to-postgres/changes-to-postgres.js
@@ -1,19 +1,36 @@
 const nano = require('nano')('http://localhost:5984')
 const Sequelize = require('sequelize')
+const async = require('async')
 
+// Need to update so that it uses the Symbol based operators.
 const sqlDbToUpdate = new Sequelize('postgres://node_user@localhost:5432/updatedDb')
+
+// Need to add model for `users` pg table.
+// Need to add model for `updatedData` pg table.
 
 const dbToWatch = nano.use('watch_these_changes')
 
 let feed = nano.db.follow('watch_these_changes', { since: 'now' })
 
 feed.on('change', (change) => {
-  dbToWatch.get(change.id, (err, body) => {
-    // Should check to see what's in the sql database for this document and insert
-    // any new records.
-    //
-    // Currently having a problem with Postgres:
-    // SequelizeConnectionError: no pg_hba.conf entry for host '10.0.2.2",
-    // user "node_user", database "updatedDb", SSL off
-  })
+  async.waterfall([
+    (callback) => {
+      dbToWatch.get(change.id, (err, body) => {
+        // Gets the current doc associated with the change.
+        // Pass doc to callback.
+      })
+    },
+    (callback, doc) => {
+      // Check if the _id is present in pg `users`.
+      // If not present, insert into pg `users`.
+      // Pass doc to callback.
+    },
+    (callback, doc) => {
+      // Grab the most recent value from pg `updatedData`.
+      // Insert any observations that are not present.
+      // Pass nothing to callback.
+    }
+  ])
 })
+
+feed.follow()
